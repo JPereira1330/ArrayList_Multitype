5cf7c6f6db8cd8290b433521d77104ca6b761565
José C. Pereira Nyex <jpereira1330@gmail.com>
2019-03-15 12:49:38 -0300

    Commit

--- arraylist.h
+++ arraylist.h
@@ -12,6 +12,18 @@
 extern "C" {
 #endif
 
+#define TYPE_INT 1
+#define TYPE_FLOAT 2
+#define TYPE_DOUBLE 3
+#define TYPE_CHAR 4
+#define TYPE_STRING 5
+    
+    typedef struct line *LINE;
+    
+    LINE newLine();
+    
+    LINE newLineObj(void *value, int type);
+    
     LINE addLine(LINE al, LINE new);
     
     void printLine(LINE al);
@@ -19,10 +31,23 @@ extern "C" {
     int getLineSize(LINE al);
     
     LINE clearLine(LINE al);
+    
+    int getLineSizeOf();
+    
+    int getLineType(LINE al);
+    
+    LINE setLineType(LINE al, int type);
+    
+    LINE getLineNext(LINE al);
+    
+    LINE setLineNext(LINE al, LINE next);
+    
+    void *getLineValue(LINE al);
 
+    LINE setLineValue(LINE al, void *value);
+    
 #ifdef	__cplusplus
 }
 #endif
 
 #endif	/* ARRAYLIST_H */
-
--- main.c
+++ main.c
@@ -2,21 +2,16 @@
  * File:   main.c
  * Author: José C. Pereira
  *
- * Created on 13 de Março de 2019, 00:13
+ * Created on 15 de Março de 2019, 12:39
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "typeline.h"
 #include "arraylist.h"
-#include "file.h"
 
-/*
- * 
- */
 int main(int argc, char** argv) {
-    
+
     LINE al1 = newLineObj( 4, TYPE_INT);
     LINE al2 = newLineObj( 3, TYPE_INT);
     LINE al3 = newLineObj( 2, TYPE_INT);
@@ -27,13 +22,6 @@ int main(int argc, char** argv) {
     al1 = addLine(al1, al4);
 
     printLine(al1);
-    
-    fileWrite(al1);
-    
-    printf("-=-=-=-");
-    
-    //LINE new = fileLoad();
-    //printLine(new);
     
     return (EXIT_SUCCESS);
 }
